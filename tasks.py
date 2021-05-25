1c39d32a4fe3ee333f610d9bb12e416a1c2bcd55
drewp@bigasterisk.com <drewp@bigasterisk.com>
2021-05-24 22:10:03 -0700

    move rules into configmap

--- tasks.py
+++
@@ -1,4 +0,0 @@
-from invoke import task
-@task
-def rebuild_config(ctx):
-    ctx.run('/my/proj/ansible/4layer_kube/manifests/cluster_curl http://prometheus.default.svc.cluster.local/-/reload -X POST', pty=True)
